564538c683153cb425b9fad9ff9dacc75eae5489
t420babe <t420babe@gmail.com>
2020-11-22 11:56:42 -0500

    Move shader compilation to  impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,10 @@
 use mat4;
 use std::collections::HashMap;
 use wasm_bindgen::{prelude::*, JsCast};
-use web_sys::{
-  WebGl2RenderingContext, WebGlBuffer, WebGlProgram, WebGlShader, WebGlUniformLocation,
-};
+use web_sys::{WebGl2RenderingContext, WebGlBuffer};
 
-pub struct ProgramInfo {
-  program: WebGlProgram,
-  attrib_locations: HashMap<String, i32>,
-  uniform_locations: HashMap<String, Option<WebGlUniformLocation>>,
-}
+pub mod program_info;
+pub use program_info::ProgramInfo;
 
 #[wasm_bindgen(start)]
 pub fn start() -> Result<(), JsValue> {
@@ -20,51 +15,7 @@ pub fn start() -> Result<(), JsValue> {
 
   let gl_context = canvas.get_context("webgl2")?.unwrap().dyn_into::<WebGl2RenderingContext>()?;
 
-  let vert_source = r#"
-    attribute vec4 a_vertex_position;
-    attribute vec4 a_vertex_color;
-
-    uniform mat4 u_model_view_matrix;
-    uniform mat4 u_projection_matrix;
-
-    varying lowp vec4 v_color;
-
-    void main(void) {
-      gl_Position = u_projection_matrix * u_model_view_matrix * a_vertex_position;
-      v_color = a_vertex_color;
-    }
-  "#;
-
-  let frag_source = r#"
-    varying lowp vec4 v_color;
-
-    void main() {
-      gl_FragColor = v_color;
-    }
-  "#;
-  let shader_program = init_shader_program(&gl_context, &vert_source, &frag_source)?;
-  let mut attrib_locations: HashMap<String, i32> = HashMap::new();
-  attrib_locations.insert(
-    "a_vertex_position".into(),
-    gl_context.get_attrib_location(&shader_program, "a_vertex_position"),
-  );
-
-  attrib_locations.insert(
-    "a_vertex_color".into(),
-    gl_context.get_attrib_location(&shader_program, "a_vertex_color"),
-  );
-
-  let mut uniform_locations: HashMap<String, Option<WebGlUniformLocation>> = HashMap::new();
-  uniform_locations.insert(
-    "u_model_view_matrix".into(),
-    gl_context.get_uniform_location(&shader_program, "u_model_view_matrix"),
-  );
-  uniform_locations.insert(
-    "u_projection_matrix".into(),
-    gl_context.get_uniform_location(&shader_program, "u_projection_matrix"),
-  );
-
-  let program_info = ProgramInfo { program: shader_program, attrib_locations, uniform_locations };
+  let program_info = ProgramInfo::new(&gl_context)?;
 
   let mut buffers: HashMap<String, WebGlBuffer> = HashMap::new();
   let vertices_buffer = init_buffers(&gl_context)?;
@@ -169,7 +120,7 @@ pub fn draw_scene(
 
   // Set the drawing position to the "identity", which is the center of the scene
   let mut model_view_matrix: [f32; 16] = mat4::new_identity();
-  let mut model_view_matrix_clone: [f32; 16] = model_view_matrix.clone();
+  let model_view_matrix_clone: [f32; 16] = model_view_matrix.clone();
 
   // Move the drawing position to where we want to start drawing the square
   // (destination matrix, matrix to translate, amount to translate)
@@ -254,61 +205,4 @@ pub fn init_color_buffer(gl_context: &WebGl2RenderingContext) -> Result<WebGlBuf
   }
 
   Ok(colors_buffer)
-}
-
-pub fn init_shader_program(
-  gl_context: &WebGl2RenderingContext,
-  vert_sourrce: &str,
-  frag_source: &str,
-) -> Result<WebGlProgram, String> {
-  // Load shaders
-  let vert_shader = load_shader(gl_context, vert_sourrce, WebGl2RenderingContext::VERTEX_SHADER)?;
-  let frag_shader = load_shader(gl_context, frag_source, WebGl2RenderingContext::FRAGMENT_SHADER)?;
-
-  // Create the shader program
-  let shader_program =
-    gl_context.create_program().ok_or_else(|| String::from("Unable to create shader object"))?;
-  gl_context.attach_shader(&shader_program, &vert_shader);
-  gl_context.attach_shader(&shader_program, &frag_shader);
-  gl_context.link_program(&shader_program);
-
-  if gl_context
-    .get_program_parameter(&shader_program, WebGl2RenderingContext::LINK_STATUS)
-    .as_bool()
-    .unwrap_or(false)
-  {
-    Ok(shader_program)
-  } else {
-    Err(
-      gl_context
-        .get_program_info_log(&shader_program)
-        .unwrap_or_else(|| String::from("Unknown error creating program object")),
-    )
-  }
-}
-
-pub fn load_shader(
-  gl_context: &WebGl2RenderingContext,
-  shader_source: &str,
-  shader_type: u32,
-) -> Result<WebGlShader, String> {
-  let shader = gl_context
-    .create_shader(shader_type)
-    .ok_or_else(|| String::from("Unable to create shader object"))?;
-  gl_context.shader_source(&shader, shader_source);
-  gl_context.compile_shader(&shader);
-
-  if gl_context
-    .get_shader_parameter(&shader, WebGl2RenderingContext::COMPILE_STATUS)
-    .as_bool()
-    .unwrap_or(false)
-  {
-    Ok(shader)
-  } else {
-    Err(
-      gl_context
-        .get_shader_info_log(&shader)
-        .unwrap_or_else(|| String::from("Unknown error creating shader")),
-    )
-  }
 }
---
+++ src/program_info.rs
@@ -0,0 +1,114 @@
+use std::collections::HashMap;
+use wasm_bindgen::prelude::*;
+use web_sys::{WebGl2RenderingContext, WebGlProgram, WebGlShader, WebGlUniformLocation};
+
+const VERT_SOURCE: &str = r#"
+    attribute vec4 a_vertex_position;
+    attribute vec4 a_vertex_color;
+
+    uniform mat4 u_model_view_matrix;
+    uniform mat4 u_projection_matrix;
+
+    varying lowp vec4 v_color;
+
+    void main(void) {
+      gl_Position = u_projection_matrix * u_model_view_matrix * a_vertex_position;
+      v_color = a_vertex_color;
+    }
+  "#;
+
+const FRAG_SOURCE: &str = r#"
+    varying lowp vec4 v_color;
+
+    void main() {
+      gl_FragColor = v_color;
+    }
+"#;
+
+pub struct ProgramInfo {
+  pub program: WebGlProgram,
+  pub attrib_locations: HashMap<String, i32>,
+  pub uniform_locations: HashMap<String, Option<WebGlUniformLocation>>,
+}
+
+impl ProgramInfo {
+  pub fn new(gl_context: &WebGl2RenderingContext) -> Result<Self, JsValue> {
+    let shader_program = init_shader_program(&gl_context)?;
+    let mut attrib_locations: HashMap<String, i32> = HashMap::new();
+
+    attrib_locations.insert(
+      "a_vertex_position".into(),
+      gl_context.get_attrib_location(&shader_program, "a_vertex_position"),
+    );
+
+    attrib_locations.insert(
+      "a_vertex_color".into(),
+      gl_context.get_attrib_location(&shader_program, "a_vertex_color"),
+    );
+
+    let mut uniform_locations: HashMap<String, Option<WebGlUniformLocation>> = HashMap::new();
+    uniform_locations.insert(
+      "u_model_view_matrix".into(),
+      gl_context.get_uniform_location(&shader_program, "u_model_view_matrix"),
+    );
+    uniform_locations.insert(
+      "u_projection_matrix".into(),
+      gl_context.get_uniform_location(&shader_program, "u_projection_matrix"),
+    );
+
+    Ok(ProgramInfo { program: shader_program, attrib_locations, uniform_locations })
+  }
+}
+
+fn init_shader_program(gl_context: &WebGl2RenderingContext) -> Result<WebGlProgram, String> {
+  // Load shaders
+  let vert_shader = load_shader(gl_context, VERT_SOURCE, WebGl2RenderingContext::VERTEX_SHADER)?;
+  let frag_shader = load_shader(gl_context, FRAG_SOURCE, WebGl2RenderingContext::FRAGMENT_SHADER)?;
+
+  // Create the shader program
+  let shader_program =
+    gl_context.create_program().ok_or_else(|| String::from("Unable to create shader object"))?;
+  gl_context.attach_shader(&shader_program, &vert_shader);
+  gl_context.attach_shader(&shader_program, &frag_shader);
+  gl_context.link_program(&shader_program);
+
+  if gl_context
+    .get_program_parameter(&shader_program, WebGl2RenderingContext::LINK_STATUS)
+    .as_bool()
+    .unwrap_or(false)
+  {
+    Ok(shader_program)
+  } else {
+    Err(
+      gl_context
+        .get_program_info_log(&shader_program)
+        .unwrap_or_else(|| String::from("Unknown error creating program object")),
+    )
+  }
+}
+
+fn load_shader(
+  gl_context: &WebGl2RenderingContext,
+  shader_source: &str,
+  shader_type: u32,
+) -> Result<WebGlShader, String> {
+  let shader = gl_context
+    .create_shader(shader_type)
+    .ok_or_else(|| String::from("Unable to create shader object"))?;
+  gl_context.shader_source(&shader, shader_source);
+  gl_context.compile_shader(&shader);
+
+  if gl_context
+    .get_shader_parameter(&shader, WebGl2RenderingContext::COMPILE_STATUS)
+    .as_bool()
+    .unwrap_or(false)
+  {
+    Ok(shader)
+  } else {
+    Err(
+      gl_context
+        .get_shader_info_log(&shader)
+        .unwrap_or_else(|| String::from("Unknown error creating shader")),
+    )
+  }
+}
